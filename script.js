c15e2266d004554382c9fb8a122231c09ee999f1
rrshttgr <rrshttgr@gmail.com>
2024-12-10 23:15:19 +0530

    Intial commmit

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ let startGame = 0;
 let level = 1;
 let index = 0;
 let gameStack = [];
-const pickRandom = [".green", ".red", ".yellow", ".blue"];
+const pickRandom = ["green", "red", "yellow", "blue"];
+
+const gameOverSound = new Audio("soundEffect/game-over.wav");
+gameOverSound.preload = "auto";
+
+const userClickSound = new Audio("soundEffect/user-click.wav");
+userClickSound.preload = "auto";
 
 function userInput(colorInput) {
+  userClickSound.play();
   if (startGame === 0) {
     startLevel();
     return;
@@ -28,7 +35,7 @@ function addRandomColor() {
 
 // Verifies user input against the game sequence
 function verifyInput(color) {
-  if (`.${color}` === gameStack[index]) {
+  if (color === gameStack[index]) {
     index++;
     if (index === gameStack.length) {
       level++;
@@ -50,26 +57,24 @@ function resetGame() {
   gameStack = [];
 }
 
-//blink effect for button
-function blinkButton(btnSelector) {
-  const button = document.querySelector(btnSelector);
+// Blink effect for button
+function blinkButton(color) {
+  const button = document.getElementById(color);
 
   button.style.boxShadow = "0 0 0 6px rgba(255, 255, 255, 0.8)";
-
   setTimeout(() => {
     button.style.boxShadow = "";
   }, 250);
 }
 
-//game lost and game reset
+// Game lost and game reset
 function gameLost() {
+  //Sound effect game over
+  gameOverSound.play();
   document.querySelector("#message").innerHTML =
     "Game Over, Press Any Key to Restart";
-  startGAme = 0;
-  level = 1;
-
-  document.querySelector("body").style.backgroundColor = "red";
+  document.body.style.backgroundColor = "red";
   setTimeout(() => {
-    document.querySelector("body").style.backgroundColor = "rgb(7, 10, 31)";
+    document.body.style.backgroundColor = "rgb(7, 10, 31)";
   }, 150);
 }
